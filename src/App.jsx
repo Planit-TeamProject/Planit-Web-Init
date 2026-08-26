c7d23f0d4e4dbcf5da1ba0ea76434bec6fab82dd
Haenglim <Haenglim@Mikhails-MacBook-Pro.local>
2026-08-26 16:33:33 +0900

    라벤더로즈 색상/폰트 적용, 무료로 시작하기 버튼 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import LandingPage from "./pages/LandingPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
 
 // LandingPage는 onNavigate(path) 콜백을 받도록 만들어뒀어서,
 // 여기서 react-router의 navigate 함수를 그대로 연결해주기만 하면 됨.

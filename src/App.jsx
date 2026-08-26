d86a6c539a050b788dba982957b4af24a22de5dc
Haenglim <Haenglim@Mikhails-MacBook-Pro.local>
2026-08-26 14:37:32 +0900

    초기화면 React 프로젝트 추가

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+
+// LandingPage는 onNavigate(path) 콜백을 받도록 만들어뒀어서,
+// 여기서 react-router의 navigate 함수를 그대로 연결해주기만 하면 됨.
+function LandingRoute() {
+  const navigate = useNavigate();
+  return <LandingPage onNavigate={navigate} />;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingRoute />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,8 @@
+// TODO: 인증 담당자(김동호)가 실제 회원가입 화면으로 교체할 자리.
+export default function SignupPage() {
+  return (
+    <div>
+      <p>회원가입 화면 (placeholder)</p>
+    </div>
+  );
+}
